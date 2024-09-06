8f70f3bbb8aada87b751f95662f22971ed5e5703
Kailas Manikam <kailashraina9@gmail.com>
2024-09-06 14:44:45 +0530

    js pratice files

---
+++ main.js
@@ -0,0 +1,39 @@
+var a=10
+var b=20
+console.log(a+b)
+
+/*
+{
+let a =10
+}
+console.log(a)
+*/
+
+/*
+{
+let a =10
+console.log(a)
+}
+
+*/
+
+/*
+{
+var a =10
+}
+console.log(a)
+*/
+
+/*
+const a=10
+a=20
+console.log(a)
+*/
+
+/*
+var fruit="apple"
+var count=10
+var price = 200
+var total=count * price
+console.log(total)
+*/

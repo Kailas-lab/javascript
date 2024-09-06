0106861674e870f61d555469f414b31e8bb312e3
Kailas Manikam <kailashraina9@gmail.com>
2024-09-06 19:20:54 +0530

    js practice files

---
+++ js DOM/random.js
@@ -0,0 +1,16 @@
+/*
+var a=Math.random()
+console.log(a)
+
+var a=Math.random()
+console.log(a*10)
+
+var b=Math.floor(2.99)
+console.log(b)
+
+var a=Math.random()
+console.log(Math.floor(a*10))
+
+var a=Math.random()
+console.log(Math.floor(a*10)+1)
+*/

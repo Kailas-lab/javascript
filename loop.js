b2f73576ea2d1d3553f66209664ecdeb47067bf2
Kailas Manikam <kailashraina9@gmail.com>
2024-09-08 12:43:08 +0530

    js task files

--- loop.js
+++ loop.js
@@ -9,3 +9,9 @@ for (var c = 1; c <= 5; c = c + 2)
         console.log("ganesh");
     }
 
+var fruit=["apple","orange","grape","pine"]
+
+for (var c = 0; c <= 3; c = c + 1) 
+    {
+        console.log(fruit[c]);
+    }
